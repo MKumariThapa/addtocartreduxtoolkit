6bd50735aa2c2d4b652d86b440adb9b34bf7f26d
mMay03 <yamkumari.thapa03@gmail.com>
2023-01-05 10:50:39 +0545

    first commit

---
+++ src/productData.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    title: "MY ADIDAS ",
+    price: 700.0,
+    img: "https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=759&q=80",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "Mexico Shoe",
+    price: 600.0,
+    img: "https://images.unsplash.com/photo-1553440070-7b7e896ed9e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=748&q=80",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Sandals",
+    price: 500.0,
+    img: "https://images.unsplash.com/photo-1577908143141-af8a2f4d1827?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Blue Shoes",
+    price: 400.0,
+    img: "https://images.unsplash.com/photo-1584432383638-6d7e15c03f75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Pinks Shoes",
+    price: 4000.0,
+    img: "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMDY1MTIxN3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    quantity: 1,
+  },
+];
